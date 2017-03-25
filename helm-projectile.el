b7553347228671f73ed5defd808c67d77c1a4b72
Kåre Slettnes <kaare@slettnes.org>
2017-03-25 23:33:14 +0100

    Make line truncation in helm buffers customizable. (#74)
    Utilize existing truncate-lines vars from helm where appropriate.

--- helm-projectile.el
+++ helm-projectile.el
@@ -63,6 +63,16 @@
 
 (defvar helm-projectile-current-project-root)
 
+(defcustom helm-projectile-truncate-lines nil
+  "Truncate lines in helm projectile commands when non--nil.
+
+Some helm-projectile commands have similar behavior with existing
+Helms.  In these cases their respective custom var for truncation
+of lines will be honored.  E.g. `helm-buffers-truncate-lines'
+dictates the truncation in `helm-projectile-switch-to-buffer'."
+  :group 'helm-projectile
+  :type 'boolean)
+
 ;;;###autoload
 (defcustom helm-projectile-fuzzy-match t
   "Enable fuzzy matching for Helm Projectile commands.
@@ -654,13 +664,16 @@ CANDIDATE is the selected file.  Used when no file is explicitly marked."
   "Default sources for `helm-projectile'."
   :group 'helm-projectile)
 
-(defmacro helm-projectile-command (command source prompt &optional not-require-root)
+(defmacro helm-projectile-command (command source prompt &optional not-require-root truncate-lines-var)
   "Template for generic helm-projectile commands.
 COMMAND is a command name to be appended with \"helm-projectile\" prefix.
 SOURCE is a Helm source that should be Projectile specific.
 PROMPT is a string for displaying as a prompt.
 NOT-REQUIRE-ROOT specifies the command doesn't need to be used in a
-project root."
+project root.
+TRUNCATE-LINES-VAR is the symbol used dictate truncation of lines.
+Defaults is `helm-projectile-truncate-lines'."
+  (unless truncate-lines-var (setq truncate-lines-var 'helm-projectile-truncate-lines))
   `(defun ,(intern (concat "helm-projectile-" command)) (&optional arg)
      "Use projectile with Helm for finding files in project
 
@@ -675,6 +688,7 @@ With a prefix ARG invalidates the cache first."
            (helm-boring-file-regexp-list nil))
        (helm :sources ,source
              :buffer "*helm projectile*"
+             :truncate-lines ,truncate-lines-var
              :prompt (projectile-prepend-project-name ,prompt)))))
 
 (helm-projectile-command "switch-project" 'helm-source-projectile-projects "Switch to project: " t)
@@ -682,7 +696,7 @@ With a prefix ARG invalidates the cache first."
 (helm-projectile-command "find-file-in-known-projects" 'helm-source-projectile-files-in-all-projects-list "Find file in projects: " t)
 (helm-projectile-command "find-dir" helm-source-projectile-directories-and-dired-list "Find dir: ")
 (helm-projectile-command "recentf" 'helm-source-projectile-recentf-list "Recently visited file: ")
-(helm-projectile-command "switch-to-buffer" 'helm-source-projectile-buffers-list "Switch to buffer: ")
+(helm-projectile-command "switch-to-buffer" 'helm-source-projectile-buffers-list "Switch to buffer: " nil helm-buffers-truncate-lines)
 (helm-projectile-command "browse-dirty-projects" 'helm-source-projectile-dirty-projects "Select a project: " t)
 
 (defun helm-projectile--files-display-real (files root)
@@ -714,6 +728,7 @@ With a prefix ARG invalidates the cache first."
                        :persistent-action #'helm-projectile-file-persistent-action
                        :persistent-help "Preview file")
             :buffer "*helm projectile*"
+            :truncate-lines helm-projectile-truncate-lines
             :prompt (projectile-prepend-project-name "Find file: ")))))
 
 ;;;###autoload
@@ -743,6 +758,7 @@ Other file extensions can be customized with the variable `projectile-other-file
                                :persistent-action #'helm-projectile-file-persistent-action
                                :persistent-help "Preview file")
                     :buffer "*helm projectile*"
+                    :truncate-lines helm-projectile-truncate-lines
                     :prompt (projectile-prepend-project-name "Find other file: ")))))
       (error "No other file found"))))
 
@@ -825,7 +841,7 @@ If it is nil, or ack/ack-grep not found then use default grep command."
      :default-directory default-directory
      :keymap helm-grep-map
      :history 'helm-grep-history
-     :truncate-lines t)))
+     :truncate-lines helm-grep-truncate-lines)))
 
 ;;;###autoload
 (defun helm-projectile-on ()
@@ -974,6 +990,7 @@ If invoked outside of a project, displays a list of known projects to jump."
   (let ((helm-ff-transformer-show-only-basename nil))
     (helm :sources helm-projectile-sources-list
           :buffer "*helm projectile*"
+          :truncate-lines helm-projectile-truncate-lines
           :prompt (projectile-prepend-project-name (if (projectile-project-p)
                                                        "pattern: "
                                                      "Switch to project: ")))))
