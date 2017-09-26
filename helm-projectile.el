4466adbcada8be19ec97b9dba52e12b8da3da849
Tianxiang Xiong <tianxiang.xiong@fundingcircle.com>
2017-09-26 11:23:49 -0700

    Use `helm-read-string` instead of `read-string`
    Avoid showing `[Display not ready!]`. Address #78 from a presentation perspective.

--- helm-projectile.el
+++ helm-projectile.el
@@ -889,7 +889,7 @@ DIR is the project root, if not set then current directory is used"
 ;;;###autoload
 (defun helm-projectile-ag (&optional options)
   "Helm version of projectile-ag."
-  (interactive (if current-prefix-arg (list (read-string "option: " "" 'helm-ag--extra-options-history))))
+  (interactive (if current-prefix-arg (list (helm-read-string "option: " "" 'helm-ag--extra-options-history))))
   (if (require 'helm-ag nil t)
       (if (projectile-project-p)
           (let* ((grep-find-ignored-files (cl-union (projectile-ignored-files-rel) grep-find-ignored-files))
