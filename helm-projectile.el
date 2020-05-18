db0b59c972fd461ef89131dacb9859f89a3934d7
Bozhidar Batsov <bozhidar@batsov.com>
2020-05-18 09:29:20 +0300

    Bump the Projectile dep

--- helm-projectile.el
+++ helm-projectile.el
@@ -7,7 +7,7 @@
 ;; Created: 2011-31-07
 ;; Keywords: project, convenience
 ;; Version: 0.14.0
-;; Package-Requires: ((helm "1.9.9") (projectile "0.14.0") (cl-lib "0.3"))
+;; Package-Requires: ((helm "1.9.9") (projectile "2.1.0") (cl-lib "0.3"))
 
 ;; This file is NOT part of GNU Emacs.
 
