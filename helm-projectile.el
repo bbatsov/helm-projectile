e56bf2da2eb9e5670884ecca9e86271989e55dd0
Bozhidar Batsov <bozhidar@batsov.com>
2020-06-15 08:27:08 +0300

    Bump the dev version

--- helm-projectile.el
+++ helm-projectile.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/bbatsov/helm-projectile
 ;; Created: 2011-31-07
 ;; Keywords: project, convenience
-;; Version: 1.0.0
+;; Version: 1.1.0-snapshot
 ;; Package-Requires: ((helm "1.9.9") (projectile "2.1.0") (cl-lib "0.3"))
 
 ;; This file is NOT part of GNU Emacs.
